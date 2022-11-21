bd3c473121ed326fb2f6e7f81398dd9522d71b3a
Ravleen-192 <meravleen@gmail.com>
2022-11-21 11:12:37 +0530

    removed gql api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "siriusxmgqlas": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
